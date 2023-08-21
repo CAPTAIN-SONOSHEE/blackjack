744d98d541daa40f1bd8247d46bb589ffdcda576
CAPTAIN-SONOSHEE <coulibaly-ablo@hotmail.fr>
2023-08-21 22:39:57 +0200

    First version without the cards deleting

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-############### Blackjack Project #####################
-
 import random
 from art import logo
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
 
 launch = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
 
@@ -22,6 +9,7 @@ while not (launch == "y" or launch == "n"):
 if launch == "y":
 
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  remaining_cards = []
   user_cards = []
   computer_cards = []
   user_score = 0
@@ -40,9 +28,11 @@ if launch == "y":
     print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
 
   def user_turn():
-    show_hands()
     if current_score(user_cards) == 21:
-        end_score(current_score(user_cards),current_score(computer_cards))
+        computer_turn()
+        return
+    
+    show_hands()
     another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
 
     while not (another_card == "y" or another_card == "n"):
@@ -50,6 +40,7 @@ if launch == "y":
 
     if another_card == "y":
       user_cards.append(int(random.choice(cards)))
+      #remove_cards(user_cards)
       if current_score(user_cards) > 21:
         if 11 in user_cards:
           user_cards[user_cards.index(11)] = 1
@@ -65,6 +56,7 @@ if launch == "y":
   def computer_turn():
     while current_score(computer_cards) < 17: 
      computer_cards.append(int(random.choice(cards)))
+     #remove_cards(computer_cards)
      if 11 in computer_cards:
        computer_cards[computer_cards.index(11)] = 1
       
@@ -81,116 +73,31 @@ if launch == "y":
 
   def end_score(user_end_score, computer_end_score):
     show_final_hands()
-    if (computer_end_score <= 21 and computer_end_score > user_end_score) or (user_end_score > 21 and computer_end_score < user_end_score) or user_end_score > 21:
+    if (computer_end_score <= 21 and computer_end_score > user_end_score) or (computer_end_score <= 21 and user_end_score > 21) :
       print("You lose.")
-    elif (user_end_score <= 21 and user_end_score > computer_end_score) or (computer_end_score > 21 and user_end_score < computer_end_score) or computer_end_score > 21:  
+    elif (user_end_score <= 21 and user_end_score > computer_end_score) or (user_end_score <= 21 and computer_end_score > 21):  
       print("You Win !")
-    elif user_end_score == 21:
+    elif not computer_end_score == 21 and user_end_score == 21:
       print("Blackjack ! You Win !!!")
-    elif (computer_end_score == user_end_score) or (computer_end_score > 21 and user_end_score > 21):
+    else:
       print("Draw")
+    print(cards)  
 
-  
+
+  #def remove_cards(cards_list):
+   # print(cards_list)
+    #for card in cards_list:
+     # if card in cards:
+      # cards.remove(card)
+    #print(cards)   
+
   def blackjack():
     user_cards.append(int(random.choice(cards)))
     user_cards.append(int(random.choice(cards)))
+    #remove_cards(user_cards)
     computer_cards.append(int(random.choice(cards)))
+    #remove_cards(computer_cards)
     user_turn()
     
-
-  
-  
-  
   print(logo)
   blackjack()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements: 
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created: 
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
-
